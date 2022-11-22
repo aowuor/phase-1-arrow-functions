8a5e19e2401267c61ba1282d25c0b496a9c536dc
aowuor <angelaowuor@gmail.com>
2022-11-22 23:58:23 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+let divide = function(){
+    const num1 = 2000;
+    const num2 = 100
+    return num1 / num2;
+}
+console.log(divide());
+
+const square = num1 => num1 * num1;
+
+const add = (num1, num2) => num1 + num2;
